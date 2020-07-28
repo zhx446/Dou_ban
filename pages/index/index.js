7dbf664401740cd570ec7d8c5e8a275a04bb417f
zhx446 <2553268591@qq.com>
2020-07-28 19:31:26 +0800

    7-28 19.31

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,30 @@ const app = getApp()
 
 Page({
     data: {
-        movieList:[{
-            
-        },{},{}]
+        movieMian: [{
+            movieImg: '../images/movie.jpg',
+            movieTitle: '寻梦环游记',
+            score: 5.7
+        }, {
+            movieImg: '../images/movie.jpg',
+            movieTitle: '阿甘正传',
+            score: 5.7
+        }, {
+            movieImg: '../images/movie.jpg',
+            movieTitle: '少年的你',
+            score: 5.7
+        }, {
+            movieImg: '../images/movie.jpg',
+            movieTitle: '烟花',
+            score: 5.7
+        }],
+        doubanList: [{
+            hotShowTxt: '影院热映'
+        }, {
+            hotShowTxt: '豆瓣热门'
+        }, {
+            hotShowTxt: '近期热门剧集'
+        }]
     },
 
     seeMore: function(e) {
